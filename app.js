09a0701dfe3df17a49a0687913c0adcb80152535
Jessica Jin <jessicajin@Jessicas-MacBook-Pro.local>
2017-01-22 13:05:36 -0800

    First commit

---
+++ app.js
@@ -0,0 +1,32 @@
+
+// 1) Nd to create an Array or Object with the stored Registered info
+
+
+	// Grabs the inputted username & password 
+	var user = document.getElementById('user').value;
+	var pw = document.getElementById('pw').value;
+
+	// stores input from register form into the function
+	function store() {
+		localStorage.setItem('user', user.value)
+		localStorage.setItem('pw', user.pw)
+		console.log(store())
+	}
+		// need to figure out what "localStorage" does...
+
+// 2) Nd to create a function to loop through the array we just created and make sure it's in the array
+
+
+
+// 3) return ERROR or LOGGED IN
+
+
+
+
+
+
+// var myArray = [user, pw]
+// console.log(myArray)
+
+
+
